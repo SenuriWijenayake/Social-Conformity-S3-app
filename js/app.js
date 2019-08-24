580d2f6dd49809630e9ddc858585e384f407634a
Senuri Wijenayake <senuri.wijenayake@gmail.com>
2019-08-24 20:04:37 +1000

    changed the api end point

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('app', []);
-//var api = 'https://stark-sands-60128.herokuapp.com';
-var api = 'http://localhost:5000';
+var api = 'https://mysterious-badlands-68636.herokuapp.com';
+// var api = 'http://localhost:5000';
 
 app.controller('BigFiveController', function($scope, $http, $window) {
   $http({
